b470c930e23c9d5459ef90c9658d23ca60bf24a2
Ronak1926 <ronaksojitra1926@gmail.com>
2025-03-31 12:28:24 +0530

    react-router-dom project

---
+++ react-router/src/index.js
@@ -0,0 +1,9 @@
+// import {Home} from './components/Home/Home.jsx'
+// import {Header} from './components/Header/Header.jsx'
+// import {Footer} from './components/Footer/Footer.jsx'
+// import {About} from './components/About/About.jsx'
+// import {Contact} from './components/Contact/Contact.jsx'
+// import {Github,githubInfoLoader} from './components/Github/Github.jsx'
+// import {User} from './components/User/User.jsx'
+
+// export {Home, Header, Footer,About,Contact,Github,User,githubInfoLoader}
